c6c3571ab8e94c2b4d395c0fe86957f5029aa752
my0366 <rlacnrrnrhd@naver.com>
2023-03-02 09:15:06 +0900

    Initial Commit

---
+++ WantedPreOnBoardingChallenge/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WantedPreOnBoardingChallenge
+//
+//  Created by 성제 on 2023/03/02.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
